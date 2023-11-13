f106bee09deec87db1d3f159f42171e655349bb3
Daniel Santiago Ramirez <dramirezroc@unal.edu.co>
2023-11-13 18:34:33 -0500

    prueba de gitignore

--- Recipy/settings.py
+++ Recipy/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -75,6 +76,7 @@ WSGI_APPLICATION = 'Recipy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 pymysql.install_as_MySQLdb()
 DATABASES = {
     'default': {
@@ -123,14 +125,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR, 'login/static')
 ]
 
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = 'login'  # Replace '/login/' with the desired login URL
+
 LOGIN_REDIRECT_URL = 'logout'
+
