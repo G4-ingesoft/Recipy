4aadf29e83a5c1053f53d1ca4eae0dc9341f5454
Amoralesco <amoralesco@unal.edu.co>
2023-11-13 18:06:09 -0500

    images load

--- Recipy/settings.py
+++ Recipy/settings.py
@@ -125,10 +125,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR, 'login/static')
 ]
 
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -136,3 +143,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_REDIRECT_URL = 'logout'
+
