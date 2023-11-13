5e5247f0b3c56564bc054c2d9e8ce48b1d90150f
Amoralesco <amoralesco@unal.edu.co>
2023-11-12 22:25:17 -0500

    minimum register/login functionality

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView,LogoutView
+
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', LoginView.as_view(template_name='login/login.html'), name='login'),
+    path('logout', LogoutView.as_view(template_name='login/logout.html'), name='logout'),
     path('register', views.register,name='register')
+
+
 ]
