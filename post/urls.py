10152b76b2a228bf48dbc60b974d19d3bb4181d6
Juan David Vásquez Pinzón <juandavidvasquezpinzon@gmail.com>
2023-11-26 19:23:37 -0500

    Guardado temporal mientras reviso el error de Fernando

--- post/urls.py
+++ post/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
-
+from .views import RecetaListView, RecetaDetailView
 
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
-    path('feed', views.feed, name='feed'),
+    # path('feed', views.feed, name='feed'),
+    path('feed/', RecetaListView.as_view(), name='feed'),
+    path('receta/<int:pk>/', RecetaDetailView.as_view(), name='receta-detail'),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- post/views.py
+++ post/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView
 
 from .models import Receta
-
-
 
 # Create your views here.
 @login_required
@@ -11,3 +10,13 @@ def feed(request):
     receta = Receta.objects.all()
     context = {'receta': receta}
     return render(request,'feed.html',context)
+
+class RecetaListView(ListView):
+    model = Receta
+    template_name = 'feed.html'
+    context_object_name = 'receta'
+    # ordering = ['-timestamp']
+
+class RecetaDetailView(ListView):
+    model = Receta
+    template_name = 'receta_detail.html'
