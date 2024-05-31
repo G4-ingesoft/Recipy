25ea2de0e35dce231759d14712cc6276723cec8c
Juan David Vásquez Pinzón <juandavidvasquezpinzon@gmail.com>
2024-05-30 20:23:34 -0500

    Recapcha para register

--- login/forms.py
+++ login/forms.py
@@ -1,19 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django_recaptcha.fields import ReCaptchaField  # Import the ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV2Checkbox  # Import the ReCaptchaWidget
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
     password1 = forms.CharField(label='contraseña',widget=forms.PasswordInput)
     password2 = forms.CharField(label=' confirma contraseña',widget=forms.PasswordInput)
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)  # Add the reCAPTCHA field
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'captcha']  # Include the reCAPTCHA field in the form
         help_texts={k:"" for k in fields}
+
         def __init__(self, *args, **kwargs):
             super(UserRegistrationForm, self).__init__(*args, **kwargs)
             self.fields['email'].widget.attrs['class'] = 'form-control col-md-6'
             #self.fields['email'].widget.attrs.update({'my_attribute_key':'my_attribute_value'})
-
-
--- login/views.py
+++ login/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth import login
 
+
 # Create your views here.
 
 
@@ -24,34 +25,39 @@ from django.contrib.auth import login
 
 def register(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        confirm_password = request.POST['confirm_password']
-        agree_terms = request.POST.get('agree_terms', False)
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            email = request.POST['email']
+            password = request.POST['password']
+            confirm_password = request.POST['confirm_password']
+            agree_terms = request.POST.get('agree_terms', False)
 
-        # Check if passwords match
-        if password != confirm_password:
-            messages.error(request, 'Las contrasenas no coinciden')
-            return redirect('register')  # Redirect to the registration page
+            # Check if passwords match
+            if password != confirm_password:
+                messages.error(request, 'Las contrasenas no coinciden')
+                return redirect('register')  # Redirect to the registration page
 
-        # Check if the terms and conditions are agreed
-        if not agree_terms:
-            messages.error(request, 'Please agree to the terms and conditions.')
-            return redirect('register')  # Redirect to the registration page
+            # Check if the terms and conditions are agreed
+            if not agree_terms:
+                messages.error(request, 'Please agree to the terms and conditions.')
+                return redirect('register')  # Redirect to the registration page
 
-        # Create a new user
-        try:
-            user = User.objects.create_user(username=username, email=email, password=password)
-            user.save()
-            messages.success(request, 'Account created successfully. You can now log in.')
+            # Create a new user
+            try:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                messages.success(request, 'Account created successfully. You can now log in.')
 
-            login(request,user)
-            return redirect('create_profile')  # Redirect to the login page
-        except Exception as e:
-            messages.error(request, f'Error creating account: {e}')
+                login(request,user)
+                return redirect('create_profile')  # Redirect to the login page
+            except Exception as e:
+                messages.error(request, f'Error creating account: {e}')
+        else:
+            messages.error(request, 'Error creating account. Please try again.')
 
-    return render(request, 'login/register.html')  # Adjust the template path accordingly
+    form = UserRegistrationForm()
+    return render(request, 'login/register.html', {'form': form})  # Adjust the template path accordingly
 
 @login_required
 def feed(request):
